22f3eaa94ee87d0bec42fdb97630132eb5325730
Dang0v <helvian1056392632@gmail.com>
2023-10-05 17:24:27 -0400

    update hw5 test

--- hw5/make_website.py
+++ hw5/make_website.py
@@ -3,9 +3,9 @@ def surround_block(tag, text):
     Surrounds the given text with the given html tag and returns the string.
     """
 
-    html = "<" + tag + ">" + text + "</" + tag + ">"
+    html_text = "<" + tag + ">" + text + "</" + tag + ">"
 
-    return html
+    return html_text
 
 def create_email_link(email_address):
     """
@@ -19,42 +19,40 @@ def create_email_link(email_address):
     Note: If, for some reason the email address does not contain @,
     use the email address as is and don't replace anything.
     """
+    address_at_replace = email_address.replace("@", "[aT]")
 
-    # insert code
-    pass
+    html_email = "<a href=\"mailto:" + email_address + "\">" + address_at_replace + "</a>"
 
-def generate_html(txt_input_file, html_output_file):
+    return html_email
+
+def load_in_lines(input_file):
     """
-    Loads given txt_input_file,
-    gets the name, email address, list of projects, and list of courses,
-    then writes the info to the given html_output_file.
-
-    # Hint(s):
-    # call function(s) to load given txt_input_file
-    # call function(s) to get name
-    # call function(s) to get email address
-    # call function(s) to get list of projects
-    # call function(s) to get list of courses
-    # call function(s) to write the name, email address, list of projects, and list of courses to the given html_output_file
+    Return the file in lines.
     """
+    f = open(input_file)
+    return f.readlines()
 
-    f = open(txt_input_file, "r")
-    resume = f.readlines()
-    # print(resume)
-
-    # get name
-    # name will always be written at the top
-    # print(resume[0])
-    name = resume[0]
+def get_name(input_file):
+    """
+    Get the name from the input file.
+    The name should always at the top of files.
+    Return "Invalida Name" if it's invalid
+    """
+    # name will always at the top
+    name = input_file[0]
     # check the first character
     if name[0].islower():
         name = "Invalid Name"
     else:
-        name = name[:-2]
-    # print(name)
+        name = name.strip("\n")
+    
+    return name
 
-    # searching for indexes
-    for i, lines in enumerate(resume):
+def get_section_indexes(input_file):
+    """
+    Find the line index for each sections.
+    """
+    for i, lines in enumerate(input_file):
         if "Courses" in lines:
             course_index = i
         elif "Projects" in lines:
@@ -64,46 +62,131 @@ def generate_html(txt_input_file, html_output_file):
         elif "@" in lines:
             email_index = i
 
-    # print(course_index)
-    # print(project_index_start)
-    # print(project_index_stop)
-    # print(email_index)
+    return course_index, project_index_start, project_index_stop, email_index
 
-    # get email address
-    email = resume[email_index].strip(" ")
-    email = email[:-1]
-    # TODO: email validation
-    # print(email)
-
-    # get courses
-    # fromatting courses
+def get_email(input_file, email_index):
+    """
+    Fine the email line, validate and return.
+    If it is invalid, return an empty string.
+    """
+    email = input_file[email_index].strip(" ")
+    email = email.strip("\n")
+    # validation
+    # check for numbers
+    email_parts = email.split("@")
+    for i in email_parts:
+        for j in i:
+            if j.isnumeric():
+                return ""
+    # check for lowercase after @     
+    for i in email_parts[1]:
+        if i.islower() == False:
+            if i != ".":
+                return ""
+    # check whether end with .edu or .com
+    if email_parts[1][-4:] == ".edu" or email_parts[1][-4:] == ".com":
+        return email
+    else:
+        return ""
+    
+def get_courses(input_file, course_index):
+    """
+    Get courses from input file.
+    Return a course list.
+    """
+    courses = []
+    # split into a list
+    course_temp = input_file[course_index].split(",")
     # normalize the first course
-    course = resume[course_index].split(",")
-    course[0] = course[0].replace("Courses", "")
+    course_temp[0] = course_temp[0].replace("Courses", "")
     # discard all non-alphabet character in the front
     # https://www.w3schools.com/python/ref_func_enumerate.asp
-    for i, course_single in enumerate(course):
+    # fromatting courses
+    for i, course_single in enumerate(course_temp):
         for j, char in enumerate(course_single):
             if char.isalpha():
                 # the last course will have "\n" at the end
-                if course_single == course[-1]:
-                    course[i] = course_single[j: -2]
+                if course_single == course_temp[-1]:
+                    # https://www.w3schools.com/python/ref_string_rstrip.asp
+                    courses.append(course_single[j:-1].rstrip(" "))
                     break
-                course[i] = course_single[j:]
+                courses.append(course_single[j:].rstrip(" "))
                 break
-    # print(course)
 
-    # get project
-    project = resume[project_index_start : project_index_stop]
-    for i, project_single in enumerate(project):
-        for j, char in enumerate(project):
+    return courses
+
+def get_project(input_file, project_index_start, project_index_end):
+    project = []
+    project_temp = input_file[project_index_start : project_index_end]
+    for i, project_single in enumerate(project_temp):
+        for j, char in enumerate(project_single):
             if char.isalpha():
-                project[i] = project_single[j:-2]
+                project.append(project_single[j:-1].rstrip())
+                break
 
-    print(project)
-    
+    return project
 
+
+def generate_html(txt_input_file, html_output_file):
+    """
+    Loads given txt_input_file,
+    gets the name, email address, list of projects, and list of courses,
+    then writes the info to the given html_output_file.
+
+    # Hint(s):
+    # call function(s) to write the name, email address, list of projects, and list of courses to the given html_output_file
+    """
+    # load the file
+    resume = load_in_lines(txt_input_file)
+
+    # get name
+    name = get_name(resume)
+
+    # searching for indexes
+    course_index, project_index_start, project_index_end, email_index = get_section_indexes(resume)
+
+    # get email address
+    email = get_email(resume, email_index)
+
+    # get courses
+    courses = get_courses(resume, course_index)
     
+    # get project
+    project = get_project(resume, project_index_start, project_index_end)
+    
+    # read the html templet into memory
+    html = load_in_lines(html_output_file)[:-2]
+
+    # basic information section
+    html.append("<div>\n")
+    html.append(surround_block("h1", name) + "\n")
+    html.append("<p>Email:" + create_email_link(email) + "</p>\n")
+    html.append("</div>\n")
+
+    # project section
+    html.append("<div>\n")
+    html.append(surround_block("h2", "Projects") + "\n")
+    html.append("<ul>\n")
+    for i in project:
+        html.append(surround_block("li", i) + "\n")
+    html.append("</ul>\n")
+    html.append("</div>\n")
+
+    # courses section
+    html.append("<div>\n")
+    html.append(surround_block("h3", "Courses") + "\n")
+    for i in courses:
+        html.append(surround_block("span", i) + "\n")
+    html.append("</div>\n")
+
+    # ending of html file
+    html.append("</div>\n") # ? ?
+    html.append("</body>\n")
+    html.append("</html>\n")
+
+    # write to file
+    f = open("test.html", "w")
+    f.writelines(html)
     
 
 
--- hw5/make_website_test-1.py
+++ hw5/make_website_test-1.py
@@ -2,6 +2,11 @@ import unittest
 
 from make_website import *
 
+resume = load_in_lines("resume.txt")
+resume_projects_w_whitespace = load_in_lines("TestResumes/resume_projects_w_whitespace/resume.txt")
+resume_project_with_blanks = load_in_lines("TestResumes/resume_projects_with_blanks/resume.txt")
+
+
 class MakeWebsite_Test(unittest.TestCase):
 
     def test_surround_block(self):
@@ -38,6 +43,96 @@ class MakeWebsite_Test(unittest.TestCase):
             '<a href="mailto:lbrandon.at.seas.upenn.edu">lbrandon.at.seas.upenn.edu</a>',
             create_email_link('lbrandon.at.seas.upenn.edu')
         )
+
+    def test_get_name(self):
+
+        # test a valid name 1
+        self.assertEqual(
+            "I.M. Student", get_name(["I.M. Student\n"])
+        )
+
+        # test a valid name, with last name in lower case
+        self.assertEqual(
+            "I.M. student", get_name(["I.M. student\n"])
+        )
+
+        # test an invalid name, with last name in lower case
+        self.assertEqual(
+            "Invalid Name", get_name(["brandon Krakowsky\n"])
+        )
+    
+    def test_section_indexes(self):
+
+        # test given resume index
+        self.assertEqual(
+            (1, 3, 5, 6), get_section_indexes(resume)
+        )
+
+        self.assertEqual(
+            (1, 3, 8, 9), get_section_indexes(resume_projects_w_whitespace)
+        )
+
+        self.assertEqual(
+            (1, 3, 8, 9), get_section_indexes(resume_project_with_blanks)
+        )
+
+
+    def test_get_email(self):
+
+        # test an invalid email with number in 1st part
+        self.assertEqual(
+            "", get_email(["lbrandon2@wharton.upenn.edu\n"], 0)
+        )
+
+        # test an invalid email with number in 2nd part
+        self.assertEqual(
+            "", get_email(["lbrandon@wharton2.upenn.edu\n"], 0)
+        )
+
+        # test a valid email
+        self.assertEqual(
+            "lbrandon@wharton.upenn.edu", get_email(["lbrandon@wharton.upenn.edu\n"], 0)
+        )
+
+    def test_get_courses(self):
+
+        # test give resume
+        self.assertEqual(
+            ['Programming Languages and Techniques', 'Biomedical image analysis', 'Software Engineering'],
+            get_courses(["Courses :- Programming Languages and Techniques," +
+                        "Biomedical image analysis, Software Engineering\n"], 0)
+        )
+
+        self.assertEqual(
+            ['CIT590', 'AB120'],
+            get_courses(["Courses - CIT590, AB120\n"], 0)
+        )
+
+        self.assertEqual(
+            ['CIT590', 'AB120'],
+            get_courses(["Courses --=-==           CIT590       , AB120          \n"], 0)
+        )
+    
+    def test_get_project(self):
+
+        self.assertEqual(
+            ['CancerDetector.com, New Jersey, USA - Project manager, codified the assessment and mapped it to the CancerDetector ontology. Member of the UI design team, designed the portfolio builder UI and category search pages UI. Reviewed existing rank order and developed new search rank order approach.', 
+             'Biomedical Imaging - Developed a semi-automatic image mosaic program based on SIFT algorithm (using Matlab)'],
+            get_project(resume, 3, 5)
+        )
+
+        self.assertEqual(
+            ['CancerDetector.com, New Jersey, USA - Project manager, codified the assessment and mapped it to the CancerDetector ontology. Member of the UI design team, designed the portfolio builder UI and category search pages UI. Reviewed existing rank order and developed new search rank order approach.', 
+             'Biomedical Imaging - Developed a semi-automatic image mosaic program based on SIFT algorithm (using Matlab)'],
+            get_project(resume_projects_w_whitespace, 3, 8)
+        )
+
+        self.assertEqual(
+            ['CancerDetector.com, New Jersey, USA - Project manager, codified the assessment and mapped it to the CancerDetector ontology. Member of the UI design team, designed the portfolio builder UI and category search pages UI. Reviewed existing rank order and developed new search rank order approach.', 
+             'Biomedical Imaging - Developed a semi-automatic image mosaic program based on SIFT algorithm (using Matlab)'],
+            get_project(resume_project_with_blanks, 3, 8)
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
